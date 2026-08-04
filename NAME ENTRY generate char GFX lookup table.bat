66548bed1fffe754f7b886b5c71331bd362a187e
But Thou Must <dqmanualscans@gmail.com>
2026-08-04 11:25:48 -0400

    Fix process for creating gfx lookup table for name entry

--- NAME ENTRY generate char GFX lookup table.bat
+++ NAME ENTRY generate char GFX lookup table.bat
@@ -1,3 +1,3 @@
 :: create the file if it does not already exist
 copy /y NUL "font/list of chars for name entry.bin" >> NUL
-atlas "font/list of chars for name entry.bin" "script/ATLAS name entry char GFX lookup table.txt"
+.\tools\atlas.exe "font/list of chars for name entry.bin" "script/ATLAS name entry char GFX lookup table.txt"
